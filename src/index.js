9c9fef692bdb64b0c5d5e7fb65bf888b1341aea7
santonix <121695750+santonix@users.noreply.github.com>
2024-05-19 07:48:36 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {
   res.send('<h1>This is first basic node JS app with Jenkins - 3rd changed made<h1>')
